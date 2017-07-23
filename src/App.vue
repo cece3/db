c72c575b81cb39f9426372182a11f9210f380d75
cece3 <christina.cece@gmail.com>
2017-07-23 18:02:48 -0500

    Adding todolist vue.js example

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
+    <order-form v-bind:services="services"></order-form>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
+import OrderForm from './components/OrderForm'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo,
+    OrderForm
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }],
+      services: [{
+        name: 'Web Development',
+        price: 300,
+        active: true
+      },
+      {
+        name: 'Design',
+        price: 400,
+        active: false
+      },
+      {
+        name: 'Integration',
+        price: 250,
+        active: false
+      },
+      {
+        name: 'Training',
+        price: 220,
+        active: false
+      }]
+    }
+  },
+  methods: {
+    createTodo (newTodo) {
+      this.todos.push(
+        newTodo
+      )
+    }
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+Vue.filter('currency', function (value) {
+  return '$' + value.toFixed(2)
+})
+
+Vue.component('alert', {
+  props: ['type', 'bold', 'msg'],
+  template: '<div class="alert {{ type }}" role="alert"><b>{{ bold }}</b> {{ msg }}</div>'
+})
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
